daf9c251d3f0c9aef7ba7d086d6649c56c908f42
lazy sheep top.1 <97109499+mkbk78@users.noreply.github.com>
2024-08-06 18:09:28 +0800

    Update download_pfam.py

--- download_pfam.py
+++ download_pfam.py
@@ -12,51 +12,53 @@ logger = logging.getLogger(__name__)
 
 
 def download_pfam_entry(base_url: str, download_file: str):
-    # disable SSL verification to avoid config issues
+    # 禁用SSL验证以避免配置问题
     context = ssl._create_unverified_context()
     HEADER_SEPARATOR = "|"
     LINE_LENGTH = 80
 
-    next = base_url
+    next = base_url# 将next设置为base_url，以便从API的第一个页面开始获取数据
 
     with open(download_file, "w") as f:
 
-        attempts = 0
-        while next:
+        attempts = 0# 初始化尝试次数为0
+        while next:# 当next不为空时，继续循环
             try:
                 req = request.Request(next, headers={"Accept": "application/json"})
-                res = request.urlopen(req, context=context)
-                # If the API times out due a long running query
+                # 创建一个请求，指定URL和Accept头，以获取JSON数据。
+                res = request.urlopen(req, context=context)# 使用urlopen打开请求，并读取响应
+                # 如果API超时，可能是由于查询运行时间过长
                 if res.status == 408:
-                    # wait just over a minute
+                    # 等待超过一分钟
                     sleep(61)
-                    # then continue this loop with the same URL
+                    # 然后继续此循环使用相同的URL
                     continue
                 elif res.status == 204:
-                    # no data so leave loop
+                    # 没有数据，跳出
                     break
                 payload = json.loads(res.read().decode())
                 next = payload["next"]
-                attempts = 0
+                attempts = 0 # 将尝试次数重置为0
             except HTTPError as e:
                 if e.code == 408:
                     sleep(61)
                     continue
                 else:
-                    # If there is a different HTTP error, it wil re-try 3 times before failing
-                    if attempts < 3:
+                    if attempts < 3:# 如果出现其他HTTP错误，将重试3次后失败
                         attempts += 1
                         sleep(61)
                         continue
-                    else:
+                    else:# 如果尝试次数达到3次，仍然无法获取数据，则抛出异常。
                         sys.stderr.write("LAST URL: " + next)
                         raise e
 
-            for i, item in enumerate(payload["results"]):
+            for i, item in enumerate(payload["results"]):# 如果尝试次数达到3次，仍然无法获取数据，则抛出异常。
 
                 entries = None
+                # 如果item中存在"entry_subset"键，则将对应的值赋给entries变量。
                 if "entry_subset" in item:
                     entries = item["entry_subset"]
+                # 如果item中存在"entries"键，则将对应的值赋给entries变量。
                 elif "entries" in item:
                     entries = item["entries"]
 
@@ -82,7 +84,7 @@ def download_pfam_entry(base_url: str, download_file: str):
                             for entry in entries
                         ]
                     )
-                    f.write(
+                    f.write(# 将entries_header、item中的"metadata"字典的"accession"和"name"键对应的值写入文件。
                         ">"
                         + item["metadata"]["accession"]
                         + HEADER_SEPARATOR
@@ -92,14 +94,14 @@ def download_pfam_entry(base_url: str, download_file: str):
                         + "\n"
                     )
                 else:
-                    f.write(
+                    f.write(# 将item中的"extra_fields"字典的"sequence"键对应的值写入文件
                         ">"
                         + item["metadata"]["accession"]
                         + HEADER_SEPARATOR
                         + item["metadata"]["name"]
                         + "\n"
                     )
-
+                # 将item中的"extra_fields"字典的"sequence"键对应的值写入文件
                 seq = item["extra_fields"]["sequence"]
                 fastaSeqFragments = [
                     seq[0 + i : LINE_LENGTH + i]
@@ -108,27 +110,24 @@ def download_pfam_entry(base_url: str, download_file: str):
                 for fastaSeqFragment in fastaSeqFragments:
                     f.write(fastaSeqFragment + "\n")
 
-            # Don't overload the server, give it time before asking for more
-            if next:
+
+            if next:# 如果next不为空，等待一秒钟再继续。
                 sleep(1)
 
 
 def main(args):
-    """
-    Downloads the sequences of the Pfam families given in the arguments.
-    """
-
     base_url = "https://www.ebi.ac.uk:443/interpro/api/protein/UniProt/entry/pfam/{}/?page_size=200&extra_fields=sequence"
-    os.makedirs("downloads", exist_ok=True)
+    #pfam的url
+    os.makedirs("downloads", exist_ok=True)#创建文件夹存放下载fasta文件
 
-    for pfam_code in args.pfam_codes:
-        if not re.match(r"PF[0-9]{5}", pfam_code):
+    for pfam_code in args.pfam_codes:# 将entries_header、item中的"metadata"字典的"accession"和"name"键对应的值写入文件。
+        if not re.match(r"PF[0-9]{5}", pfam_code):# 如果Pfam代码不符合格式要求（必须以"PF"开头，后跟5个数字），则抛出异常。
             raise Exception(f'Pfam code not valid. Must be "PF" followed by 5 digits, got: {pfam_code}. Example: PF12345')
-        download_file = os.path.join("downloads", f"{pfam_code}.fasta")
-        url = base_url.format(pfam_code)
-        logger.info(f"Downloading {pfam_code} from {url}")
-        download_pfam_entry(url, download_file)
-        logger.info(f"Downloaded {pfam_code} to {download_file}")
+        download_file = os.path.join("downloads", f"{pfam_code}.fasta")# 创建一个名为download_file的字符串，其中包含Pfam代码对应的下载文件名。
+        url = base_url.format(pfam_code)# 创建一个名为url的字符串，其中包含根据Pfam代码生成的API URL。
+        logger.info(f"Downloading {pfam_code} from {url}")# 打印一条日志，表示正在下载Pfam代码对应的蛋白质序列。
+        download_pfam_entry(url, download_file)# 调用download_pfam_entry函数，下载Pfam代码对应的蛋白质序列并将其保存到文件中。
+        logger.info(f"Downloaded {pfam_code} to {download_file}")# 打印一条日志，表示已经下载了Pfam代码对应的蛋白质序列并将其保存到文件中。
 
 
 if __name__ == "__main__":
@@ -136,7 +135,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "pfam_codes",
         nargs="+",
-        help='Pfam codes of the families to be downloaded. Must be "PF" and 5 digits. Example: PF12345',
+        help='pfam——codes格式要满足“PF”+五位数字. Example: PF12345',
     )
     args = parser.parse_args()
     main(args)
