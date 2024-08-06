a2619c8db75efa27018706e1825534ec47c34b2c
lazy sheep top.1 <97109499+mkbk78@users.noreply.github.com>
2024-08-06 18:51:05 +0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -25,103 +25,84 @@ def sample(
     temperature: float = 1.0,
     top_k: Optional[int] = None,
 ) -> list[str]:
-    """
-    Generate samples from the model given a prompt. Using top-k sampling with temperature.
-    """
-    model.eval()
+    #从模型中生成样本，使用 top-k 采样和温度参数。
+    model.eval()# 将模型设置为评估模式
+    if isinstance(prompt, str):# 如果 prompt 是字符串
+        encoding: Encoding = tokenizer.encode(prompt)# 使用分词器将 prompt 编码为 token ids
+        ids = torch.tensor(encoding.ids)
+        ids = ids[:len(torch.nonzero(ids))]# 移除 padding
 
-    if isinstance(prompt, str):
-        encoding: Encoding = tokenizer.encode(prompt)
-        ids = torch.tensor(encoding.ids)                             # (T,)
-        ids = ids[:len(torch.nonzero(ids))]
-
-        x = torch.zeros((num_return_sequences, ids.shape[0]))        # (B, T)
-        x = x + ids
-        x = x.to(device).to(torch.int32)
-    # prompt is a tensor of token ids, with shape (B, T), in case of bidi smapling
-    elif isinstance(prompt, torch.Tensor):
-        x = prompt.to(device).to(torch.int32)                        # (B, T)
-    else:
+        x = torch.zeros((num_return_sequences, ids.shape[0]))# 创建一个形状为 (num_return_sequences, ids.shape[0]) 的张量，用于存储生成的样本
+        x = x + ids# 将 prompt 的 token ids 复制到生成的样本中
+        x = x.to(device).to(torch.int32)# 将生成的样本移动到设备上
+    
+    elif isinstance(prompt, torch.Tensor):# 如果 prompt 是一个张量
+      
+        x = prompt.to(device).to(torch.int32)     # 将生成的样本移动到设备上 
+    else:# 如果 prompt 既不是字符串也不是张量，则抛出 ValueError
         raise ValueError("Prompt should be either string or torch.Tensor")
 
-    past_key_values = None
-    generated = x
+    past_key_values = None  # 初始化 past_key_values
+    generated = x   # 初始化生成的样本
 
-    pbar = tqdm(total=max_length - generated.shape[-1])
-    while generated.shape[-1] < max_length:
-        # using cached attn outputs from previous iterations
-        output = model(x, past_key_values=past_key_values)
-        past_key_values = output.past_key_values
-        logits = output.logits                                       # (B, T, V)
-        # get logits only for the last token
-        logits = logits[:, -1, :]                                    # (B, V)
-        logits = logits / temperature
-        if top_k is not None:
-            v, _ = torch.topk(logits, top_k, dim=-1)                 # (B, k)
-            logits = torch.where(logits >= v[:, -1:], logits, -1e9)  # (B, V)
-        probs = torch.softmax(logits, dim=-1)                        # (B, V)
-        x = torch.multinomial(probs, num_samples=1)                  # (B, 1)
-        generated = torch.cat([generated, x], dim=-1)                # (B, T+1)
-        pbar.update()
-    pbar.close()
-
-    decoded = [tokenizer.decode(row.detach().cpu().numpy().tolist()) for row in generated]
-    return decoded
+    pbar = tqdm(total=max_length - generated.shape[-1])     # 创建一个进度条，显示总共有多少个 token 需要生成
+    while generated.shape[-1] < max_length:                 # 当生成的样本长度小于最大长度时，继续生成
+        output = model(x, past_key_values=past_key_values)  # 使用缓存的注意力输出
+        past_key_values = output.past_key_values            # 更新 past_key_values
+        logits = output.logits                              # 获取 logits                                 
+        logits = logits[:, -1, :]                           # 只获取最后一个 token 的 logits                        
+        logits = logits / temperature                       # 将 logits 除以温度
+        if top_k is not None:                               # 如果 top_k 不为 None，则使用 top-k 采样
+            v, _ = torch.topk(logits, top_k, dim=-1)             # 获取 top-k 的 logits                
+            logits = torch.where(logits >= v[:, -1:], logits, -1e9)      # 将 logits 中小于 top-k 的 logits 设置为 -1e9
+        probs = torch.softmax(logits, dim=-1)          # 计算概率                
+        x = torch.multinomial(probs, num_samples=1)                 # 使用 multinomial 采样           
+        generated = torch.cat([generated, x], dim=-1)             # 将生成的样本添加到生成的样本中     
+        pbar.update()        # 更新进度条
+    pbar.close()    # 关闭进度条
+    decoded = [tokenizer.decode(row.detach().cpu().numpy().tolist()) for row in generated]    # 使用分词器将生成的样本解码为字符串
+    return decoded    # 返回解码后的字符串
 
 
 def truncate(seq: str) -> str:
-    """
-    Remove family special tokens, initial 1 or 2 token and truncate
-    the sequence to the first 1 or 2 token found.
+    #移除家族特殊标记，初始的 1 或 2 个标记，并截断序列到第一个找到的 1 或 2 个标记。以 2 开头的序列（C -> N 生成）会被反转。
     
-    Sequences begginning with 2 (C -> N generation) are reversed.
-    """
-
-    # remove family token
-    seq = re.sub(r"<\|.*\|>", "", seq)
-
-    # remove initial terminus
-    terminus = seq[0]
+    seq = re.sub(r"<\|.*\|>", "", seq)# 移除家族标记
+    terminus = seq[0]# 移除初始终止符
     seq = seq[1:]
 
-    min_1 = seq.find("1")
-    if min_1 == -1:
+    min_1 = seq.find("1") # 找到第一个 "1" 的位置
+    if min_1 == -1:# 如果找不到 "1"，则将 min_1 设置为序列的长度
         min_1 = len(seq)
 
-    min_2 = seq.find("2")
-    if min_2 == -1:
+    min_2 = seq.find("2")# 找到第一个 "2" 的位置
+    if min_2 == -1:# 如果找不到 "2"，则将 min_2 设置为序列的长度
         min_2 = len(seq)
 
-    # truncate the sequence to next terminus token
-    seq = seq[: min(min_1, min_2)]
-    if terminus == "1":
+    seq = seq[: min(min_1, min_2)]    # 截断序列到第一个找到的 "1" 或 "2" 的位置
+    if terminus == "1":    # 如果初始终止符是 "1"，则返回截断后的序列
         return seq
-    else:
+    else:    # 否则，反转截断后的序列并返回
         return seq[::-1]
 
 
-def reverse(seq: str) -> str:
-    """
-    Reverse a sequence that starts with a family token and initial terminus.
-    Then continue generating the sequence in opposite direction.
-    """
-    prefix_pattern = re.compile(r"<\|.*\|>")
-    m = re.search(prefix_pattern, seq)
-    prefix = m.group() if m else ""
-
-    # remove family token
-    seq = seq.replace(prefix, "")
-
-    # remove initial terminus and reverse the sequence
+def reverse(seq: str) -> str:    
+    #反转一个以家庭标记和初始终止符开头的序列。然后，继续在相反的方向生成序列。
+    prefix_pattern = re.compile(r"<\|.*\|>")    # 创建一个正则表达式，用于匹配家庭标记
+    m = re.search(prefix_pattern, seq)    # 在序列中搜索家庭标记
+    prefix = m.group() if m else ""    # 如果找到家庭标记，则将其保存到 prefix 变量中
+    seq = seq.replace(prefix, "")  # 移除家庭标记
+    
+    # 移除初始终止符并反转序列
     start_terminus = seq[0]
     seq = seq[1:]
     seq = seq[::-1]
 
-    # if we generated end of sequence
+    # 如果我们生成了序列的末尾
     if seq[0] in ["1", "2"]:
         seq = seq[1:]
 
-    # reverse and put opposite terminus
+    # 反转并添加相反的终止符
     if start_terminus == "1":
         return prefix + "2" + seq
     else:
@@ -129,11 +110,14 @@ def reverse(seq: str) -> str:
 
 
 def main(args):
+    # 设置环境变量
     os.environ["PYTHONHASHSEED"] = str(args.seed)
     os.environ["TOKENIZERS_PARALLELISM"] = "false"
+    # 设置随机种子
     torch.manual_seed(args.seed)
     torch.cuda.manual_seed(args.seed)
 
+    # 检查 CUDA 是否可用
     if args.device == "cuda" and not torch.cuda.is_available():
         logger.warning("CUDA not available. Falling back to CPU.")
         device = torch.device("cpu")
@@ -141,16 +125,15 @@ def main(args):
         device = torch.device(args.device)
     logger.info(f"Device: {device}")
 
+    # 如果使用 CPU 并且 batch_size 较大，则发出警告
     if str(device) == "cpu" and args.batch_size > 1:
         logger.warning(f"You are using CPU for inference with a relatively high batch size of {args.batch_size}, therefore inference might be slow. Consider using a GPU or smaller batch.")
 
-    #model = torch.jit.load("checkpoints/hugohrban/progen2-small-finetuned/e5/model.safetensors")
-    #model = torch.load("model_epoch_1.pth", map_location=device)
-
-    #print(model)
+    # 加载模型
     model = ProGenForCausalLM.from_pretrained("pretrainmodel",ignore_mismatched_sizes=True ).to(device)
     logger.debug("Model loaded.")
 
+    # 加载分词器
     logger.info("Loading tokenizer")
     tokenizer: Tokenizer = Tokenizer.from_pretrained(args.model)
     tokenizer.no_padding()
@@ -158,27 +141,34 @@ def main(args):
     logger.debug(f"Tokenizer vocab size: {tokenizer.get_vocab_size()}")
     logger.debug(f"Tokenizer vocab: {tokenizer.get_vocab()}")
 
+    # 创建样本目录
     samples_dir = os.path.join("generated_samples")
     os.makedirs(samples_dir, exist_ok=True)
+    # 创建输出文件
     output_file = os.path.join(samples_dir, f"samples_ctx{args.prompt}_k{args.k}_t{args.t}.fa")
     print(output_file)
 
+    # 如果 top_k 超过词汇表大小，则设置为 None
     if args.k == 0 or args.k > model.config.vocab_size_lm_head:
         args.k = None
 
+    # 打印采样参数
     logger.debug(f"Sampling parameters: top_k={args.k}, temperature={args.t}")
+    # 编码 prompt
     tokens = tokenizer.encode(args.prompt).tokens
     logger.info(f"Prompt tokens: {tokens}")
 
+    # 如果使用双向采样，则设置最大长度
     if args.bidirectional:
         args.max_length = (args.max_length - len(tokens)) // 2
         if len(tokens) <= 2:
             logger.warning("Prompt is too short for bidirectional sampling. Please provide a longer prompt.")
 
-    with open(output_file, "w") as f:
-        for i in range(args.iters):
+
+    with open(output_file, "w") as f:    # 打开输出文件
+        for i in range(args.iters):       # 遍历每个迭代
             logger.info(f"Sampling batch {i+1} / {args.iters}")
-            samples = sample(
+            samples = sample(      # 生成样本
                 model=model,
                 tokenizer=tokenizer,
                 device=device,
@@ -188,12 +178,12 @@ def main(args):
                 max_length=args.max_length,
                 top_k=args.k,
             )
-            if args.bidirectional:
-                reversed_samples = [reverse(s) for s in samples]
+            if args.bidirectional:            # 如果使用双向采样
+                reversed_samples = [reverse(s) for s in samples]         # 反转样本
                 samples = []
                 for rs in reversed_samples:
-                    prompt = torch.tensor(tokenizer.encode(rs).ids).view(1, -1).to(model.device)
-                    samples.extend(sample(
+                    prompt = torch.tensor(tokenizer.encode(rs).ids).view(1, -1).to(model.device)      # 编码反转后的样本
+                    samples.extend(sample(# 生成样本
                         model=model,
                         tokenizer=tokenizer,
                         device=device,
@@ -203,11 +193,13 @@ def main(args):
                         max_length=args.max_length * 2,
                         top_k=args.k,
                     ))
+            # 将样本写入文件
             for j, c in enumerate(samples):
                 print(f">seq_{i * args.batch_size + j+1}", file=f)
                 c = truncate(c)
                 print(c, file=f)
     logger.info(f"Generated samples were saved to file {output_file}.")
+
 
 
 if __name__ == "__main__":
@@ -229,35 +221,28 @@ if __name__ == "__main__":
         "--iters",
         type=int,
         default=1,
-        help="How many iterations of gerneration to run. Default: 1",
     )
     parser.add_argument(
         "--batch_size",
         type=int,
         default=8,
-        help="How many sequences to generate at one iteration. Default: 8",
     )
     parser.add_argument(
         "--max_length",
         type=int,
         default=512,
-        help="Maximum length of the generated sequence. Default: 512",
     )
     parser.add_argument(
         "--k",
         type=int,
         default=15,
-        help="Top-k sampling parameter. 0 means no top-k sampling. Default: 10",
     )
-    parser.add_argument(
-        "--t", type=float, default=1.0, help="Temperature for sampling. Default: 1.0"
-    )
+    parser.add_argument("--t", type=float, default=1.0, )
     parser.add_argument("--seed", type=int, default=42)
-    parser.add_argument("--debug", action="store_true", help="Enable debug logging level.")
+    parser.add_argument("--debug", action="store_true",)
     parser.add_argument(
         "--bidirectional",
         action="store_true", 
-        help="Enable bidirectional sampling. After generating half of the sequence, it is flipped and model generates the other half in opposite direction."
     )
     args = parser.parse_args()
 
